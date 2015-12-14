04885d50f9876ab8d9e9fae2eee215955661d550
Greg Kitson <greg.kitson@gmail.com>
2015-12-14 03:32:22 +0000

    size sort order distribution

--- fart.rb
+++ fart.rb
@@ -23,7 +23,7 @@ create table if not exists objects (
     confirmed bool
   );
 SQL
-records = db.execute('select count(id) from objects')[0]['count(id)']
+records = db.execute('select count(id) from objects').first['count(id)']
 
 if records == 0
   puts "objects.db empty, querying s3"
@@ -53,36 +53,26 @@ if records == 0
   end
 end
 
-def describe(row)
-  "#{row['size']} #{row['bucket']}/#{row['key']}"
-end
-
-def debug(msg)
-  # if debugging
-  puts msg
-end
-
+total = db.execute('select sum(size) from objects').first['sum(size)']
 biggest = db.execute('select * from objects order by size desc limit 1').first
 batch_size_max = biggest['size']
-db.execute("update objects set batch=1 where id=#{biggest['id']}")
-debug("batch 1 << #{describe(biggest)}")
+num_batches = total/batch_size_max
 
-while db.execute('select count(id) from objects where batch is null')[0]['count(id)'] > 0
-  batch = db.execute('select max(batch) from objects')[0]['max(batch)'] + 1
-  batch_size_current = 0
-  while batch_size_max >= batch_size_current
-    max_object_size = batch_size_max - batch_size_current
-    object_arr = db.execute("select * from objects where batch is null and size < #{max_object_size} limit 1")
-    break if object_arr.length == 0 # end batch if nothing else fits
-    obj = object_arr.first
-
-    db.execute("update objects set batch=#{batch} where id=#{obj['id']}")
-    batch_size_current = batch_size_current + obj['size']
-    debug("batch #{batch} << #{describe(obj)}")
+current_batch = 0
+db.execute('select * from objects where batch is null order by size desc').each do |row|
+  db.execute("update objects set batch=#{current_batch} where id=#{row['id']}")
+  print '.' # illustrate progress
+  if current_batch == num_batches
+    current_batch = 1
+  else
+    current_batch = current_batch + 1
   end
-  debug("\n\nbatch #{batch} complete, total #{batch_size_current}, remaining #{batch_size_max - batch_size_current}\n\n")
 end
+printf "\n" # illustrate progress
 
-batches = db.execute('select distinct batch from objects where batch not null order by batch')
-debug("\nbatches #{batches}")
-require 'pry'; binding.pry
+# require 'pry'; binding.pry ; exit 123
+
+(0..num_batches).each do |batch|
+  total = db.execute("select sum(size) from objects where batch=#{batch}")
+  puts "batch #{batch} total: #{total}"
+end
